7e0daf716aba97c2e0bc95c16e5dcbf1b5c6b320
molin <531679708@qq.com>
2016-11-02 17:20:25 +0800

    添加正则表达式判断，用于监控text是否满足条件

--- JCKitDemo/JCKit/Core/UIKit/UITextField/UITextField+JCTextField.m
+++ JCKitDemo/JCKit/Core/UIKit/UITextField/UITextField+JCTextField.m
@@ -9,6 +9,45 @@
 #import "UITextField+JCTextField.h"
 #import <objc/runtime.h>
 
+#define kPredicate @"Predicate"
+
+@interface _JCTextPredicateTarget : NSObject
+
+@property (nonatomic, strong) NSPredicate *predicate;
+@property (nonatomic, copy) void (^predicateBlock)(BOOL);
+
+- (instancetype)initWithPredicate:(NSString *)predicate;
+- (void)setPredicateFormat:(NSString *)predicate;
+- (void)addPredicateBlock:(void (^)(BOOL))block;
+- (void)invoke:(UITextField *)sender;
+
+@end
+@implementation _JCTextPredicateTarget
+
+- (instancetype)initWithPredicate:(NSString *)predicate {
+    if (self = [super init]) {
+        _predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",predicate];
+    }
+    return self;
+}
+
+- (void)setPredicateFormat:(NSString *)predicate {
+    _predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",predicate];
+}
+
+- (void)addPredicateBlock:(void (^)(BOOL))block {
+    _predicateBlock = [block copy];
+}
+
+- (void)invoke:(UITextField *)sender {
+    if (_predicateBlock) {
+        _predicateBlock([_predicate evaluateWithObject:sender.text]);
+    }
+}
+
+@end
+
+
 @implementation UITextField (JCTextField)
 
 /**
@@ -16,6 +55,35 @@
  */
 - (void)setPlaceholderColor:(UIColor *)placeholderColor {
     [self setValue:placeholderColor forKey:@"_placeholderLabel.textColor"];
+}
+
+- (void)addPredicate:(NSString *)predicate action:(void (^)(BOOL))block {
+    _JCTextPredicateTarget *_target = [self _actionTargets][kPredicate];
+    if (!_target) {
+        _target = [[_JCTextPredicateTarget alloc] init];
+        NSMutableDictionary *targets = [self _actionTargets];
+        targets[kPredicate] = _target;
+    }
+    
+    [_target setPredicateFormat:predicate];
+    [_target addPredicateBlock:block];
+    [self addTarget:_target action:@selector(invoke:) forControlEvents:UIControlEventEditingChanged];
+}
+
+- (void)removePredicateTarget {
+    NSMutableDictionary *targets = [self _actionTargets];
+    [targets enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        [self removeTarget:obj action:@selector(invoke:) forControlEvents:UIControlEventEditingChanged];
+    }];
+}
+
+- (NSMutableDictionary *)_actionTargets {
+    NSMutableDictionary *targets = objc_getAssociatedObject(self, _cmd);
+    if (!targets) {
+        targets = [NSMutableDictionary new];
+        objc_setAssociatedObject(self, _cmd, targets, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return targets;
 }
 
 @end
