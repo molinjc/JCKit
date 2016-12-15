e9c399329fb201a214f96e5cb8386166472c2b5b
molin <531679708@qq.com>
2016-12-15 17:31:36 +0800

    简化创建CADisplayLink，直接加到NSRunLoop

---
+++ JCKitDemo/JCKit/Core/UIKit/Quartz/CADisplayLink+JCToRunLoop.h
@@ -0,0 +1,17 @@
+//
+//  CADisplayLink+JCToRunLoop.h
+//  JCKitDemo
+//
+//  Created by molin.JC on 2016/12/15.
+//  Copyright © 2016年 molin. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CADisplayLink (JCToRunLoop)
+
++ (CADisplayLink *)displayLinkToRunLoopWithTarget:(id)target selector:(SEL)sel;
+
+- (void)stopDisplayLink;
+
+@end
