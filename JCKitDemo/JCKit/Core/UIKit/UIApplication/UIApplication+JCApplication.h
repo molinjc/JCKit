757e3151b7868bbc84d75d66384b26ccdfb59733
molin <531679708@qq.com>
2016-12-16 14:02:40 +0800

    注释说明，增加判断程序是否为从AppStore安装、 拨打电话、 隐藏键盘方法

--- JCKitDemo/JCKit/Core/UIKit/UIApplication/UIApplication+JCApplication.h
+++ JCKitDemo/JCKit/Core/UIKit/UIApplication/UIApplication+JCApplication.h
@@ -10,14 +10,26 @@
 
 @interface UIApplication (JCApplication)
 
-@property (nonatomic, readonly) NSString *appBundleName;
-@property (nonatomic, readonly) NSString *appBundleID;
-@property (nonatomic, readonly) NSString *appVersion;
-@property (nonatomic, readonly) NSString *appBuildVersion;
+@property (nonatomic, readonly) NSString *appBundleName;   // < Bundle Name >
+@property (nonatomic, readonly) NSString *appBundleID;     // < Bundle ID >
+@property (nonatomic, readonly) NSString *appVersion;      // < app版本 >
+@property (nonatomic, readonly) NSString *appBuildVersion; // < Bundle版本 >
+@property (nonatomic, readonly) BOOL isPirated;            // < 判断程序是否为从AppStore安装,否则是盗版 >
+@property (nonatomic, readonly) BOOL isBeingDebugged;      // < 是否为调试模式 >
 
 /**
  手动更改状态栏的颜色
  */
 - (void)setStatusBarBackgtoundColor:(UIColor *)color;
+
+/**
+ 拨打电话
+ */
++ (void)call:(NSString *)phone;
+
+/**
+ 隐藏键盘
+ */
++ (void)hideKeyboard;
 
 @end
