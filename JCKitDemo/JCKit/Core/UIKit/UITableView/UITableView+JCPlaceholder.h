df5ae3f489dff8fb133e314250aa3e4b0e971fcb
molin <531679708@qq.com>
2016-12-20 17:30:05 +0800

    UITableView的占位符提示

---
+++ JCKitDemo/JCKit/Core/UIKit/UITableView/UITableView+JCPlaceholder.h
@@ -0,0 +1,24 @@
+//
+//  UITableView+JCPlaceholder.h
+//  JCKitDemo
+//
+//  Created by molin.JC on 2016/12/20.
+//  Copyright © 2016年 molin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (JCPlaceholder)
+
+//**********************************
+@property (nonatomic, strong) UIImage *placeholderImage;
+
+@property (nonatomic, copy) NSString *placeholder;
+
+@property (nonatomic, copy) void (^reload)();
+//**********************************
+
+/// 使用外部自定义占位View，那上面三个属性就不能用了
+@property (nonatomic, strong) UIView *customPlaceholderView;
+
+@end
