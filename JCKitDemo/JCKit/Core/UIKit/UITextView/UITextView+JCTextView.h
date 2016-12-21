2e44ed50d133e42271d5a10dd2f595fea7e0eb28
molin <531679708@qq.com>
2016-12-21 13:29:43 +0800

    UITextView的类目，设置选中字符范围

---
+++ JCKitDemo/JCKit/Core/UIKit/UITextView/UITextView+JCTextView.h
@@ -0,0 +1,28 @@
+//
+//  UITextView+JCTextView.h
+//  JCKitDemo
+//
+//  Created by molin.JC on 2016/12/21.
+//  Copyright © 2016年 molin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (JCTextView)
+
+/**
+ 选中所有文字
+ */
+- (void)selectAllText;
+
+/**
+ 当前选中的字符串范围
+ */
+- (NSRange)currentSelectedRange;
+
+/**
+ 选中指定范围的文字
+ */
+- (void)setSelectedRange:(NSRange)range;
+
+@end
