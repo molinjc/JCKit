5a27144085e34ad2a8a8c968ef159837d3743b22
molin <531679708@qq.com>
2016-12-22 17:41:21 +0800

    UIImageView类目，倒影方法

---
+++ JCKitDemo/JCKit/Core/UIKit/UIImageView/UIImageView+JCImageView.h
@@ -0,0 +1,18 @@
+//
+//  UIImageView+JCImageView.h
+//  JCKitDemo
+//
+//  Created by molin.JC on 2016/12/22.
+//  Copyright © 2016年 molin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (JCImageView)
+
+/**
+ 倒影
+ */
+- (void)imageReflect;
+
+@end
