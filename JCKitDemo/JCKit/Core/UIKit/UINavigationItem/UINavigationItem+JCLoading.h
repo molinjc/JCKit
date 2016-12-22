914402b98b60da3964099b4396129fb9a50d5368
molin <531679708@qq.com>
2016-12-22 16:30:52 +0800

    菊花加载

---
+++ JCKitDemo/JCKit/Core/UIKit/UINavigationItem/UINavigationItem+JCLoading.h
@@ -0,0 +1,17 @@
+//
+//  UINavigationItem+JCLoading.h
+//  JCKitDemo
+//
+//  Created by molin.JC on 2016/12/22.
+//  Copyright © 2016年 molin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationItem (JCLoading)
+
+- (void)startLoadingAnimating;
+
+- (void)stopLoadingAnimating;
+
+@end
